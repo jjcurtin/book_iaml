f13c2e94bca484662c41de38bc8399786c628a27
John Curtin <jjcurtin@wisc.edu>
2024-01-23 14:54:40 -0600

    delete render_slides.R from Susan

--- render_slides.R
+++
@@ -1,9 +0,0 @@
-# to render slides, the _quarto_revealjs.yml file  must be renamed to the default _quarto.yml, and then re-named back to allow the book to render with the "real" _quarto.yml. Using this function simplifies this pipeline
-
-render_slides <- function(...) {
-  file.rename("_quarto.yml", "_quarto_book.yml")
-  file.rename("_quarto_revealjs.yml", "_quarto.yml")
-  on.exit(file.rename("_quarto.yml", "_quarto_revealjs.yml"))
-  on.exit(file.rename("_quarto_book.yml", "_quarto.yml"), add = TRUE)
-  quarto::quarto_render(..., as_job = FALSE)
-}
