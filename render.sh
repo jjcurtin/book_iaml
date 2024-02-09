7f41df7e3d7d940d4e84455ba0cfd8af4c11c3cc
John Curtin <jjcurtin@wisc.edu>
2024-02-08 20:43:13 -0600

    new files for render

--- render.sh
+++ render.sh
@@ -1,16 +1,32 @@
 #!/bin/bash  
 
+# ./render.sh FILE FORMAT
+# FILE = all or filename
+# FORMAT = book, slides, or wide_slides
+
 FILE=$1
 FORMAT=$2
 
-if [ "$FORMAT" = "book" ]  then     
-	echo "Rendering book"
+if [ "$FILE" = "all" ] && [ "$FORMAT" = "book" ]; then
+  echo "Rendering full book"
+  cp _quarto_book.yml _quarto.yml
+  quarto render
 fi
 
-if [ "$FORMAT" = "slides" ]  then     
+if [ $FILE != "all" ] && [ "$FORMAT" = "book" ];  then
+	echo "Rendering book chapter"
+  cp _quarto_book.yml _quarto.yml
+  quarto render "$FILE"
+fi
+ 
+if [ $FILE != "all" ] && [ "$FORMAT" = "slides" ];  then
 	echo "Rendering standard slides"
+  cp _quarto_slides.yml _quarto.yml
+  quarto publish quarto-pub "$FILE" 
 fi
-
-if [ "$FORMAT" = "wide_slides" ]  then     
+ 
+if [ $FILE != "all" ] && [ "$FORMAT" = "wide_slides" ];  then
 	echo "Rendering wide slides"
+  cp _quarto_slides_wide.yml _quarto.yml
+  quarto publish quarto-pub "$FILE" 
 fi
