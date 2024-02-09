19d6d7ce3720b5fd8d4b5601dc1b82bf9302ffc3
John Curtin <jjcurtin@wisc.edu>
2024-02-08 20:54:13 -0600

    test book render 004

--- render.sh
+++ render.sh
@@ -8,25 +8,27 @@ FILE=$1
 FORMAT=$2
 
 if [ "$FILE" = "all" ] && [ "$FORMAT" = "book" ]; then
-  echo "Rendering full book"
+  echo "Rendering all chapters to web book"
+  echo "Do not forget to add, commit, and push to Github"
   cp _quarto_book.yml _quarto.yml
   quarto render
 fi
 
 if [ $FILE != "all" ] && [ "$FORMAT" = "book" ];  then
-	echo "Rendering book chapter"
+	echo "Rendering $FILE to web book"
+  echo "Do not forget to add, commit, and push to Github"
   cp _quarto_book.yml _quarto.yml
   quarto render "$FILE"
 fi
  
 if [ $FILE != "all" ] && [ "$FORMAT" = "slides" ];  then
-	echo "Rendering standard slides"
+	echo "Rendering $FILE to standard slides on quarto-pub"
   cp _quarto_slides.yml _quarto.yml
   quarto publish quarto-pub "$FILE" 
 fi
  
 if [ $FILE != "all" ] && [ "$FORMAT" = "wide_slides" ];  then
-	echo "Rendering wide slides"
+	echo "Rendering $FILE to wide slides on quarto-pub"
   cp _quarto_slides_wide.yml _quarto.yml
   quarto publish quarto-pub "$FILE" 
 fi
