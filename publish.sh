419de064f03595a0d58596c4d63191dc155c49fe
John Curtin <jjcurtin@wisc.edu>
2024-03-12 21:16:37 -0500

    update publish

--- publish.sh
+++ publish.sh
@@ -9,8 +9,7 @@ FORMAT=$2
 
 if [ "$FILE" = "all" ] && [ "$FORMAT" = "book" ]; then
   echo ""
-  echo "Rendering all chapters to web book"
-  echo "Do not forget to add, commit, and push to Github"
+  echo "Publishing all chapters to gh-pages"
   echo ""
   cp _quarto_book.yml _quarto.yml
   quarto publish gh-pages --no-browser 
@@ -30,7 +29,7 @@ fi
  
 if [ $FILE != "all" ] && [ "$FORMAT" = "slides" ];  then
   echo ""
-	echo "Rendering $FILE to standard slides on quarto-pub"
+	echo "Publishing $FILE to standard slides on quarto-pub"
   echo ""
   cp _quarto_slides.yml _quarto.yml
   quarto publish quarto-pub "$FILE" 
@@ -39,7 +38,7 @@ fi
  
 if [ $FILE != "all" ] && [ "$FORMAT" = "slides_wide" ];  then
   echo ""
-	echo "Rendering $FILE to wide slides on quarto-pub"
+	echo "Publishing $FILE to wide slides on quarto-pub"
   echo ""
   cp _quarto_slides_wide.yml _quarto.yml
   quarto publish quarto-pub "$FILE" 
